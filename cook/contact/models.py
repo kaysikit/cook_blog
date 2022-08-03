765e28b7b386259c5ae87dd72104c90ce11b92d1
Denis Kaysarov <kaysik_it@mail.ru>
2022-08-03 20:47:58 +0500

    Add template about Add output socials in left_nav and footer

--- cook/contact/models.py
+++ cook/contact/models.py
@@ -1,3 +1,4 @@
+from ckeditor.fields import RichTextField
 from django.db import models
 
 class ContactModel(models.Model):
@@ -20,3 +21,31 @@ class ContactLink(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+class About(models.Model):
+    """Класс модели страницы О нас"""
+    text = RichTextField()
+    mini_text = RichTextField()
+
+    def get_first_image(self):
+        item = self.about_images.first()
+        return item.image.url
+
+    def get_images(self):
+        return self.about_images.order_by('id')[1:]
+
+
+class ImageAbout(models.Model):
+    """Класс модели изображений страницы о нас"""
+    image = models.ImageField(upload_to="about/")
+    page = models.ForeignKey(About, on_delete=models.CASCADE, related_name='about_images')
+    alt = models.CharField(max_length=100)
+
+
+class Social(models.Model):
+    """Класс модели соц. сетей страницы о нас"""
+    icon = models.FileField(upload_to="icons/")
+    name = models.CharField(max_length=200)
+    link = models.URLField()
